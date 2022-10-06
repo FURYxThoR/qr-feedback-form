d76bd6287705ada009d859b257f0ceb915089880
FURYxThoR <thepurveshpanchal@gmail.com>
2022-10-06 17:25:14 +0530

    1

--- firebase.js
+++ firebase.js
@@ -3,83 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getAuth, RecaptchaVerifier } from "firebase/auth";
 import { getAuth, signInWithPhoneNumber } from "firebase/auth";
 
-
-
 const auth = getAuth();
-auth.languageCode = 'it';
-// To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
 window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
   'size': 'invisible',
   'callback': (response) => {
     // reCAPTCHA solved, allow signInWithPhoneNumber.
     onSignInSubmit();
   }
+  
 }, auth);
 
 
-const auth = getAuth();
-window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
-
-const auth = getAuth();
-window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-  'size': 'normal',
-  'callback': (response) => {
-    // reCAPTCHA solved, allow signInWithPhoneNumber.
-    // ...
-  },
-  'expired-callback': () => {
-    // Response expired. Ask user to solve reCAPTCHA again.
-    // ...
-  }
-}, auth);
 
 const recaptchaResponse = grecaptcha.getResponse(recaptchaWidgetId);
 
 const phoneNumber = getPhoneNumberFromUserInput();
 const appVerifier = window.recaptchaVerifier;
-
-const auth = getAuth();
-signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-      // SMS sent. Prompt user to type the code from the message, then sign the
-      // user in with confirmationResult.confirm(code).
-      window.confirmationResult = confirmationResult;
-      // ...
-    }).catch((error) => {
-      // Error; SMS not sent
-      // ...
-    });
-
-    grecaptcha.reset(window.recaptchaWidgetId);
-
-// Or, if you haven't stored the widget ID:
-window.recaptchaVerifier.render().then(function(widgetId) {
-  grecaptcha.reset(widgetId);
-});
-
-const code = getCodeFromUserInput();
-confirmationResult.confirm(code).then((result) => {
-  // User signed in successfully.
-  const user = result.user;
-  // ...
-}).catch((error) => {
-  // User couldn't sign in (bad verification code?)
-  // ...
-});
-
-
-var credential = firebase.auth.PhoneAuthProvider.credential(confirmationResult.verificationId, code);
-
-firebase.auth().signInWithCredential(credential);
-
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC62kR5nxhbtVR4nikc7gMhFawU5hvJIDs",
-  authDomain: "qr-feedback-form.firebaseapp.com",
-  projectId: "qr-feedback-form",
-  storageBucket: "qr-feedback-form.appspot.com",
-  messagingSenderId: "570897041700",
-  appId: "1:570897041700:web:bff1b6b7b7353d3c274bb1"
-};
